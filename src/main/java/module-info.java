1cfe16e867b9a1f684e15c92026d4a0c02cdb0a3
Albison Bekaj <albison.bekaj@student.uni-pr.edu>
2025-05-10 13:16:03 +0200

    Hapja e Repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projekti3faza3siguridhenave {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.projekti3faza3siguridhenave to javafx.fxml;
+    exports com.example.projekti3faza3siguridhenave;
+}
