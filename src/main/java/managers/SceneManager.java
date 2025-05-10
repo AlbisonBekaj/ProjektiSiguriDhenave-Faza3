704e1f598da6d3794ecb0bc0828144b8248e2c3c
amatraci <149633690+amatraci@users.noreply.github.com>
2025-05-11 00:50:05 +0200

    importimi i librarive dhe i klases SceneManager

---
+++ src/main/java/managers/SceneManager.java
@@ -0,0 +1,17 @@
+package managers;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SceneManager {
+    private static Stage primaryStage;
+    private static final Map<String, String> scenePaths = new HashMap<>();
+    private static final Map<String, Scene> sceneCache = new HashMap<>();
+
+
+}
