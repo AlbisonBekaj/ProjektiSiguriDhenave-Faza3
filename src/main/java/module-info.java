fef8c0fb1ffd421ac11ed7b6e14a4bd7c4cbdd6a
alishoshi <ali.shoshi@student.uni-pr.edu>
2025-05-10 14:34:41 +0200

    Views

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.projekti3faza3siguridhenave {
     requires javafx.fxml;
 
 
-    opens com.example.projekti3faza3siguridhenave to javafx.fxml;
-    exports com.example.projekti3faza3siguridhenave;
+    opens Application to javafx.fxml;
+    exports Application;
 }
