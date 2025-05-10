f09d2e7e240ad31aab88d9e0f5384704f112d340
Albison Bekaj <albison.bekaj@student.uni-pr.edu>
2025-05-10 16:08:32 +0200

    Krijimi i klases User

---
+++ src/main/java/models/User.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User {
+    private int id;
+    private String userName;
+    private String salt;
+    private String saltedHash;
+
+    public User(int id, String userName, String salt, String saltedHash) {
+        this.id = id;
+        this.userName = userName;
+        this.salt = salt;
+        this.saltedHash = saltedHash;
+    }
+
+    public User getInstance(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String userName = rs.getString("userName");
+        String salt = rs.getString("salt");
+        String saltedHash = rs.getString("saltedHash");
+        return new User(id, userName, salt, saltedHash);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public String getSaltedHash() {
+        return saltedHash;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public void setSaltedHash(String slatedHash) {
+        this.saltedHash = saltedHash;
+    }
+}
