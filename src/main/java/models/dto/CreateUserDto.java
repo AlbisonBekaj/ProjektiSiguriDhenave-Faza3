cec6c4a13830f9082dad3557d1a2653b44f3bf1f
AmelaSyla <148979964+AmelaSyla@users.noreply.github.com>
2025-05-10 20:36:54 -0700

    ndryshime, importim i klasave

--- src/main/java/models/dto/CreateUserDto.java
+++ src/main/java/models/dto/CreateUserDto.java
@@ -1,17 +1,51 @@
 package models.dto;
 
-public class CreateUserDto {
-    private String username;
-    private String password;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-    public CreateUserDto(String username, String password) {
-        this.username = username;
+public class CreateUserDto {
+    private String userName;
+    private String password;
+    private String salt;
+    private String saltedHash;
+
+    public CreateUserDto(String userName, String password) {
+        this.userName = userName;
         this.password = password;
+        this.salt = generateSalt();
+        this.saltedHash = generateSaltedHash(password, salt);
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
+    private String generateSalt() {
+        SecureRandom sr = new SecureRandom();
+        byte[] saltBytes = new byte[16];
+        sr.nextBytes(saltBytes);
+        return Base64.getEncoder().encodeToString(saltBytes);
+    }
 
-    public void setUsername(String username) { this.username = username; }
-    public void setPassword(String password) { this.password = password; }
+    private String generateSaltedHash(String password, String salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(Base64.getDecoder().decode(salt));
+            byte[] hashedPassword = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedPassword);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getUserName() { return userName; }
+    public String getPassword() { return password; }
+    public String getSalt() { return salt; }
+    public String getSaltedHash() { return saltedHash; }
+
+    public void setUserName(String userName) { this.userName = userName; }
+
+    public void setPassword(String password) {
+        this.password = password;
+        this.salt = generateSalt();
+        this.saltedHash = generateSaltedHash(password, salt);
+    }
 }
