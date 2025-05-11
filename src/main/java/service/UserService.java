e761b573fa1267b1973902873569e906b21d624a
AmelaSyla <148979964+AmelaSyla@users.noreply.github.com>
2025-05-10 20:51:35 -0700

    UserService/UserSignUp

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,6 +4,15 @@ import models.User;
 import models.dto.CreateUserDto;
 
 public class UserService {
+    private final UserRepository userRepository;
 
+    public UserService() {
+        this.userRepository = new UserRepository();
+    }
+
+    public User signUp(String username, String password) {
+        CreateUserDto dto = new CreateUserDto(username, password);
+        return userRepository.create(dto);
+    }
 
 }
