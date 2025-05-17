382e1dbd0dfd6ab2ca74a43f87ec7555ca27103d
amatraci <149633690+amatraci@users.noreply.github.com>
2025-05-17 14:24:45 +0200

    krijimi i SessionManager

---
+++ src/main/java/session/SessionManager.java
@@ -0,0 +1,19 @@
+package session;
+
+import models.User;
+
+public class SessionManager {
+    private static User currentUser;
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void clear() {
+        currentUser = null;
+    }
+}
